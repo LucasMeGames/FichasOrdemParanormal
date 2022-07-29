7ba2f455d58d32ebe321c226006a2598f205a174
Lucas <ludafepi123456@gmail.com>
2022-07-29 13:15:00 -0400

    Changed from express to standalone

--- servers/main.js
+++ servers/main.js
@@ -1,10 +1,5 @@
-const express = require('express');
-const http = require('http');
 const { Server } = require("socket.io");
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server();
 
 
 function getUrlVars(url) {
@@ -14,20 +9,25 @@ function getUrlVars(url) {
     for (var i = 0; i < hashes.length; i++) {
         hash = hashes[i].split('=');
         myJson[hash[0]] = hash[1];
-        // If you want to get in native datatypes
-        // myJson[hash[0]] = JSON.parse(hash[1]);
     }
     return myJson;
 }
 
+
+
+
 io.sockets.on('connection', function(socket) {
+
     socket.on('create', function(room) {
+
         console.log(socket.handshake.headers.referer);
+
         let idficha = getUrlVars(socket.handshake.headers.referer);
+
+
         if(idficha.token === room){
             socket.join(room);console.log('connected to: ' + room);
             socket.on(room, (msg) => {
-                console.log('message sent : ' + msg.result);
                 io.emit(room, msg);
             });
         } else {
@@ -39,7 +39,6 @@ io.sockets.on('connection', function(socket) {
         });
     });
 });
-
-server.listen(3000, () => {
+io.listen(3000, () => {
     console.log('listening on *:3000');
 });
